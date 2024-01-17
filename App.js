deb549ca8fe64dc20fe0b5ef5c5dd0edf961c828
Kevin Dutzy <kddutz15@gmail.com>
2024-01-16 18:43:56 -0700

    built login component

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text style={{color:"white"}} >Pat eats ASS :(</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Login.jsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react'
+import { Image, ImageBackground, StyleSheet, Text, TextInput, View, } from 'react-native';
+import { Button } from '@rneui/themed';
+
+function Login() {
+
+    const [currentTime, setCurrentTime] = useState('')
+
+    const backgroundImage = currentTime >= 18 ? require('../assets/night-img.png') : require('../assets/morning-img.png')
+    const logo = require('../assets/logo(2).png')
+    const helper = currentTime >= 18 ? require('../assets/helper-night.png') : require('../assets/helper-day.png')
+    const buttonColor = currentTime >= 18 ? '#2c3a66b6' : '#d3845c'
+
+    useEffect(() => {
+        let hours = new Date().getHours(); //Current Hours
+        setCurrentTime(hours)
+    }, [])
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+        },
+        image: {
+            flex: 1,
+            justifyContent: 'center',
+            // width: "100%"
+        },
+        text: {
+            // margin: 8,
+            color: 'white',
+            width: "100%",
+            fontSize: 25,
+            lineHeight: 40,
+            textAlign: 'left',
+        },
+        input: {
+            backgroundColor: '#8b666680',
+            width: '75%',
+            height: 40,
+            borderWidth: 1,
+            padding: 10,
+        },
+    });
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'space-between' }}>
+            <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.image}>
+                <View style={{ width: "100%", height: "76%", display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }} >
+                    <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                        <Image source={logo} style={{ width: 325, height: 200, }} />
+                    </View>
+                    <View style={{ justifyContent: 'space-even', alignItems: 'left', width: "80%", height: "55%", backgroundColor: '#00000080' }}>
+                        <View style={{ margin: 20 }}>
+                            <Text style={styles.text}>Username</Text>
+                            <TextInput
+                                keyboardAppearance={currentTime >= 18 ? 'dark' : 'light'}
+                                style={{
+                                    backgroundColor: '#8b666680',
+                                    color: 'white',
+                                    width: 200,
+                                    height: 40,
+                                    borderWidth: 1,
+                                    padding: 10,
+                                }}
+                            />
+                        </View>
+                        <View style={{ margin: 20, }}>
+                            <Text style={styles.text}>Password</Text>
+                            <TextInput
+                                secureTextEntry={true}
+                                keyboardAppearance={currentTime >= 18 ? 'dark' : 'light'}
+                                style={{
+                                    backgroundColor: '#8b666680',
+                                    color: 'white',
+                                    width: 200,
+                                    height: 40,
+                                    marginBottom: 5,
+                                    borderWidth: 1,
+                                    padding: 10,
+                                }}
+                            />
+                        </View>
+                        <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                            <Button size='lg' title={"Login"} buttonStyle={{backgroundColor: buttonColor, borderRadius: 5, width: 125}} />
+                        </View>
+                    </View>
+                </View>
+                <View style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                    <Image source={helper} style={{ width: 150, height: 175, }} />
+                </View>
+            </ImageBackground>
+        </View>
+    )
+}
+
+
+
+export default Login
+
